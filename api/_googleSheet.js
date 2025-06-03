5d8b6c6e5b9d48c50ac8001ad5842d16b2d3b00d
yuhnat77 <dinhtanhuy547@gmail.com>
2025-06-03 19:44:03 +0700

    Create _googleSheet.js

---
+++ api/_googleSheet.js
@@ -0,0 +1,14 @@
+const { google } = require('googleapis');
+
+async function getSheetsClient() {
+  const credentials = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_KEY);
+  const auth = new google.auth.GoogleAuth({
+    credentials,
+    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+  });
+  return google.sheets({ version: 'v4', auth });
+}
+
+const SHEET_ID = process.env.GOOGLE_SHEET_ID;
+
+module.exports = { getSheetsClient, SHEET_ID };
