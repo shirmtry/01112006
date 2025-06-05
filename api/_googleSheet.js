80b27843901a9ba3e58f2c61e56a2420ce7bff71
yuhnat77 <dinhtanhuy547@gmail.com>
2025-06-05 07:10:35 +0700

    Update _googleSheet.js

--- api/_googleSheet.js
+++ api/_googleSheet.js
@@ -3,15 +3,20 @@ const { google } = require('googleapis');
 const SHEET_ID = process.env.GOOGLE_SHEET_ID;
 const SERVICE_ACCOUNT = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_KEY);
 
-function getSheetsClient() {
-  const auth = new google.auth.GoogleAuth({
+function getSheetsAuth() {
+  return new google.auth.GoogleAuth({
     credentials: SERVICE_ACCOUNT,
     scopes: ['https://www.googleapis.com/auth/spreadsheets'],
   });
+}
+
+function getSheetsClient() {
+  const auth = getSheetsAuth();
   return google.sheets({ version: 'v4', auth });
 }
 
 module.exports = {
   SHEET_ID,
+  getSheetsAuth,
   getSheetsClient,
 };
